d6f16bfa14f3bff6373184024b45e692972d4f62
LeoSallu <L.sallustio99@gmail.com>
2023-03-15 15:58:58 +0100

    Creata App+Lista con Vue

--- js/main.js
+++ js/main.js
@@ -1,2 +1,25 @@
 'use strict';
 
+// Importo Vue 
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+        list:[
+            {
+                text:'Fare la spesa',
+                done: true        
+            },
+            {
+                text:'Lavare la macchina',
+                done: true        
+            },
+            {
+                text:'Studiare Vue',
+                done: true        
+            }
+        ]
+    }
+  }
+}).mount('#app');
